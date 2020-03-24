2fe5ecc9e186464a75a1cecebb2f5b29ff82ffb3
LucasSarri <lucassarri@hotmail.com>
2020-03-24 13:06:41 -0300

    dia 2 OmniStack

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.get("/",function(req,res)
+/*
+    Rota / Recurso
+    Metodos HTTP: 
+    1- GET: Buscar uma informação do back-end
+    2- POST: criar uma informação no back-end
+    3- PUT: alterar uma informação no back-end
+    4- DELETE: Deletar uma informação do back-end
+*/
+
+app.get('/', (req,res) =>
 {
     return res.send("Alo");
+});
+
+app.get('/users', (req,res) =>
+{
+    return res.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Lucas Sarri'
+    });
 });
 
 app.listen(3333);
