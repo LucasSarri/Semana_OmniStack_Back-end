ae85162651ded19a0f79c9e1bbef06742ad7c476
LucasSarri <lucassarri@hotmail.com>
2020-03-24 15:29:45 -0300

    Dia 02 Semana OmniStack

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Semana_OmniStack
 Este repositório se trata do conteúdo abordado na Semana OmniStack 11, além de estudos externos feitos por mim sobre os conceitos citados em vídeo.
+
+Tipos de Bancos de dados:
+* SQL: SQLite, MySQL e PostgreSQL.
+* NoSQL: MongoDB.
+
+Driver: SELECT * FROM users
+Query Builder: table('users').select('*').where();
+
+Banco a ser utilizado: SQLite
+Query Builder: Knex.js
---
+++ src/routes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const routes = express.Router();
+
+/*
+    Rota / Recurso
+    Metodos HTTP: 
+    1- GET: Buscar uma informação do back-end
+    2- POST: criar uma informação no back-end
+    3- PUT: alterar uma informação no back-end
+    4- DELETE: Deletar uma informação do back-end
+    Tipos de Parâmetros:
+    1- Query Params: Parâmetros nomeados enviados na rota após o "?" (Filtros, paginação e etc)
+    const params = req.query;
+    console.log(params);
+    2- Route Parms: Parâmetros utilizados para identificar recursos
+    const params = req.params;
+    console.log(params);
+    3- Request Body (Req Body): Corpo da da requisição, usado para criar ou alterar recursos
+    const body = req.body;
+    console.log(params); 
+*/
+routes.get('/', (req,res) =>
+{
+    return res.send("Alo");
+});
+
+routes.post('/users', (req,res) =>
+{
+    return res.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Lucas Sarri'
+    });
+});
+
+module.exports = routes;
